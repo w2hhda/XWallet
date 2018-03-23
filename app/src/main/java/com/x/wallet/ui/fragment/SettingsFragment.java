04a24d4d6d8192ab0b5ee96ffdde32630caef707
wuliang <sophie_weilian@126.com>
2018-03-23 11:17:15 +0800

    [XWallet]:添加设置界面视觉

--- app/src/main/java/com/x/wallet/ui/fragment/SettingsFragment.java
+++ app/src/main/java/com/x/wallet/ui/fragment/SettingsFragment.java
@@ -1,10 +1,23 @@
 package com.x.wallet.ui.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.x.wallet.R;
 
 /**
  * Created by wuliang on 18-3-13.
  */
 
 public class SettingsFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.settings_fragment, container, false);
+        return view;
+    }
 }
