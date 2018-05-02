b2c268c7ae2d475932b25a0f36a0d7ad4026810e
张星 <xing.zhang@itel-mobile.com>
2018-05-02 14:27:43 +0800

    需求:[CoinPay]交易详情增加添加地址入口

--- app/src/main/java/com/x/wallet/ui/activity/TransactionDetailActivity.java
+++ app/src/main/java/com/x/wallet/ui/activity/TransactionDetailActivity.java
@@ -2,14 +2,20 @@ package com.x.wallet.ui.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.x.wallet.AppUtils;
 import com.x.wallet.R;
 import com.x.wallet.btc.BtcUtils;
 import com.x.wallet.lib.common.LibUtils;
+import com.x.wallet.transaction.FavoriteAddressDbAsycTask;
 import com.x.wallet.transaction.token.TokenUtils;
+import com.x.wallet.ui.ActionUtils;
+import com.x.wallet.ui.data.AddressItem;
 import com.x.wallet.ui.data.TransactionItem;
+import com.x.wallet.ui.helper.FavoriteAddressHelper;
 
 import java.math.BigDecimal;
 
@@ -18,6 +24,9 @@ import java.math.BigDecimal;
  */
 
 public class TransactionDetailActivity extends WithBackAppCompatActivity {
+    private TextView addFavoriteTv;
+    private ImageView addToIv;
+    private TextView toAddressTv;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +37,12 @@ public class TransactionDetailActivity extends WithBackAppCompatActivity {
 
     private void initView(){
         TextView fromAddressTv = findViewById(R.id.transaction_detail_from);
-        TextView toAddressTv   = findViewById(R.id.transaction_detail_to);
+        toAddressTv   = findViewById(R.id.transaction_detail_to);
         TextView amountTv      = findViewById(R.id.transaction_detail_amount);
         TextView feeTv         = findViewById(R.id.transaction_detail_fax);
         TextView hashTv        = findViewById(R.id.transaction_detail_hash);
+        addFavoriteTv = findViewById(R.id.add_to_favorite);
+        addToIv = findViewById(R.id.to_icon);
         TransactionItem item = (TransactionItem) getIntent().getSerializableExtra(AppUtils.TRANSACTION_ITEM);
         boolean isTokenAccount = getIntent().getBooleanExtra(AppUtils.ACCOUNT_TYPE, false);
 
@@ -45,6 +56,15 @@ public class TransactionDetailActivity extends WithBackAppCompatActivity {
         }
 
         hashTv.setText(item.getReceiptHash());
+        final String address = toAddressTv.getText().toString();
+        final String type = isBtcAccount() ? AppUtils.COIN_ARRAY[0] : AppUtils.COIN_ARRAY[1];
+        addFavoriteTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActionUtils.addFavoriteAddress(TransactionDetailActivity.this, address, type);
+            }
+        });
+        bindAddView();
     }
 
     private void bindAmount(TransactionItem item, boolean isTokenAccount, TextView amountTv, TextView feeTv){
@@ -89,4 +109,36 @@ public class TransactionDetailActivity extends WithBackAppCompatActivity {
     private boolean isBtcAccount(){
         return getIntent().getIntExtra(AppUtils.COIN_TYPE, -1) == LibUtils.COINTYPE.COIN_BTC;
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        bindAddView();
+    }
+
+    private void bindAddView(){
+        final AddressItem addressItem = getAddressItem();
+        final int actionType = FavoriteAddressDbAsycTask.QUERY_EXIST;
+        FavoriteAddressHelper.handleAddressAction(TransactionDetailActivity.this, actionType, addressItem, listener);
+    }
+
+    final FavoriteAddressDbAsycTask.OnDataActionFinishedListener listener = new FavoriteAddressDbAsycTask.OnDataActionFinishedListener() {
+        @Override
+        public void onDataActionFinished(boolean isSuccess) {
+            if (!isSuccess) {
+                addToIv.setVisibility(View.VISIBLE);
+                addFavoriteTv.setVisibility(View.VISIBLE);
+            }else {
+                addToIv.setVisibility(View.GONE);
+                addFavoriteTv.setVisibility(View.GONE);
+            }
+        }
+    };
+
+    private AddressItem getAddressItem(){
+        final String address = toAddressTv.getText().toString();
+        final String type = isBtcAccount() ? AppUtils.COIN_ARRAY[0] : AppUtils.COIN_ARRAY[1];
+        return new AddressItem(-1, address, type, null);
+    }
+
 }
